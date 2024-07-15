b542f6c4f637d3d042cdf0869f1f569625b8fb82
DarkAdamz <agwuuchea@gmail.com>
2024-07-15 10:27:16 +0100

    Project incomplete commited with bugs

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+//Integration between Convex and Clerk
+export default {
+    providers: [
+        {
+            domain: "https://funky-bluegill-73.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ],
+};
